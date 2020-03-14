ea7ebc9914d37875589115805c593cfa065a2e8f
Revati Tekale <revitekale1910@gmail.com>
2020-03-14 17:11:05 +0530

    REFACTORED:All three methods as a one generic method

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,36 +1,14 @@
 public class FindMaximum {
+    //Find maximum from all three different datatypes
+    public static <E extends Comparable> E checkMaximum(E first, E second, E third) {
 
-    //Maximum form three integers
-    public static Integer maximumInteger(Integer first, Integer second, Integer third) {
-        Integer maximum = first;
+        E maximum = first;
 
-        if(second.compareTo(maximum) > 0)
-            maximum = second; //second is larger
+        if (second.compareTo(maximum) > 0)
+            maximum = second;
         if (third.compareTo(maximum) > 0)
-            maximum = third; //third is larger
+            maximum = third;
+
         return maximum;
-    }
-    //Maximum from floats
-    public static Float maximumFloat(Float first, Float second, Float third) {
-        Float maximum = first;
-        if(second.compareTo(maximum) > 0)
-            maximum = second;//second is larger
-        if (third.compareTo(maximum) > 0)
-            maximum = third;//third is larger
-        return maximum;
-
-    }
-    //Maximum from strings
-    public static String largeString(String first, String second, String third) {
-        String large = first;
-
-        if (second.compareTo(large) > 0)
-            large = second;
-        if (third.compareTo(large) > 0)
-            large = third;
-        return large;
-
-
-
     }
 }
--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -7,63 +7,63 @@ public class TestMaximum {
     @Test
     public void givenIntegers_WhenMaxAtOne_ReturnMaximum() {
         maximum = new FindMaximum();
-        int maxIntegers = maximum.maximumInteger(5, 2, 3);
+        int maxIntegers = maximum.checkMaximum(5, 2, 3);
         Assert.assertEquals(5, maxIntegers);
     }
 
     @Test
     public void givenIntegers_WhenMaxAtTwo_ReturnMaximum() {
         maximum = new FindMaximum();
-        int maxIntegers = maximum.maximumInteger(2, 5, 3);
+        int maxIntegers = maximum.checkMaximum(2, 5, 3);
         Assert.assertEquals(5, maxIntegers);
     }
 
     @Test
     public void givenIntegers_WhenMaxAtThree_ReturnMaximum() {
         maximum = new FindMaximum();
-        int maxIntegers = maximum.maximumInteger(3, 3, 5);
+        int maxIntegers = maximum.checkMaximum(3, 3, 5);
         Assert.assertEquals(5, maxIntegers);
     }
     @Test
     public void givenFloats_WhenMaxAtOne_ReturnMaximum() {
         maximum = new FindMaximum();
-        Float max = maximum.maximumFloat(5.2f, 2.5f, 3.6f);
+        Float max = maximum.checkMaximum(5.2f, 2.5f, 3.6f);
         Assert.assertEquals((Float) 5.2f, max);
     }
 
     @Test
     public void givenFloats_WhenMaxATwo_ReturnMaximum() {
         maximum = new FindMaximum();
-        Float max = maximum.maximumFloat(3.6f, 5.2f, 2.5f);
+        Float max = maximum.checkMaximum(3.6f, 5.2f, 2.5f);
         Assert.assertEquals((Float) 5.2f, max);
     }
 
     @Test
     public void givenFloats_WhenMaxAtThree_ReturnMaximum() {
         maximum = new FindMaximum();
-        Float max = maximum.maximumFloat(2.5f, 3.6f, 5.2f);
+        Float max = maximum.checkMaximum(2.5f, 3.6f, 5.2f);
         Assert.assertEquals((Float) 5.2f, max);
     }
 
     @Test
     public void givenStrings_WhenMaxAtOne_ReturnMaximum() {
         maximum = new FindMaximum();
-        String max = maximum.largeString("Z", "R", "A");
-        Assert.assertEquals("Z", max);
+        String max = maximum.checkMaximum("7", "5", "3");
+        Assert.assertEquals("7", max);
     }
 
     @Test
     public void givenStrings_WhenMaxAtTwo_ReturnMaximum() {
         maximum = new FindMaximum();
-        String max = maximum.largeString("R", "Z", "A");
-        Assert.assertEquals("Z", max);
+        String max = maximum.checkMaximum("5", "7", "3");
+        Assert.assertEquals("7", max);
     }
 
     @Test
     public void givenStrings_WhenMaxAtThree_ReturnMaximum() {
         maximum = new FindMaximum();
-        String max = maximum.largeString("A", "R", "Z");
-        Assert.assertEquals("Z", max);
+        String max = maximum.checkMaximum("3", "5", "7");
+        Assert.assertEquals("7", max);
     }
 
 }
