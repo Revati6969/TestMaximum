78f74900647d892f5ec8c3ab40f10aa6f8c203f8
Revati Tekale <revitekale1910@gmail.com>
2020-03-19 14:17:21 +0530

    ADDED:Print max method to print maximum value

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -3,26 +3,29 @@ public class FindMaximum<E extends Comparable> {
     E first;
     E second;
     E third;
-
+    //Parameterized constructor
     public FindMaximum(E first, E second, E third) {
         this.first = first;
         this.second = second;
         this.third = third;
     }
-
-    public E checkMaximum() {
-        return checkMaximum(first,second, third);
+    //Print result
+    private static <E> void printMax(E result) {
+        System.out.println(result);
     }
-
-
+    //Find maximum from three values
     public static <E extends Comparable> E checkMaximum(E first, E second, E third) {
         E maximum = first;
-
         if (second.compareTo(maximum) > 0)
             maximum = second;
         if (third.compareTo(maximum) > 0)
             maximum = third;
-
         return maximum;
+    }
+
+    public E checkMaximum() {
+        E result = checkMaximum(first, second, third);
+        printMax(result);
+        return result;
     }
 }
