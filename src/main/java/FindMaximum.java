4931582471cfffb7e3681ed28cd6c451be31e394
Revati Tekale <revitekale1910@gmail.com>
2020-03-19 13:55:49 +0530

    REFACTORED:Parameterized constructor

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,7 +1,21 @@
-public class FindMaximum {
-    //Find maximum from all three different datatypes
-    public static <E extends Comparable> E checkMaximum(E first, E second, E third) {
+public class FindMaximum<E extends Comparable> {
 
+    E first;
+    E second;
+    E third;
+
+    public FindMaximum(E first, E second, E third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public E checkMaximum() {
+        return checkMaximum(first,second, third);
+    }
+
+
+    public static <E extends Comparable> E checkMaximum(E first, E second, E third) {
         E maximum = first;
 
         if (second.compareTo(maximum) > 0)
--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -6,64 +6,66 @@ public class TestMaximum {
 
     @Test
     public void givenIntegers_WhenMaxAtOne_ReturnMaximum() {
-        maximum = new FindMaximum();
-        int maxIntegers = maximum.checkMaximum(5, 2, 3);
-        Assert.assertEquals(5, maxIntegers);
+        maximum = new FindMaximum(5, 2, 3);
+        Integer max = (Integer) maximum.checkMaximum();
+        Assert.assertEquals((Integer) 5, max);
     }
 
     @Test
     public void givenIntegers_WhenMaxAtTwo_ReturnMaximum() {
-        maximum = new FindMaximum();
-        int maxIntegers = maximum.checkMaximum(2, 5, 3);
-        Assert.assertEquals(5, maxIntegers);
+        maximum = new FindMaximum(2, 5, 3);
+        Integer max = (Integer) maximum.checkMaximum();
+        Assert.assertEquals((Integer) 5, max);
     }
 
     @Test
     public void givenIntegers_WhenMaxAtThree_ReturnMaximum() {
-        maximum = new FindMaximum();
-        int maxIntegers = maximum.checkMaximum(3, 3, 5);
-        Assert.assertEquals(5, maxIntegers);
+        maximum = new FindMaximum(2, 3, 5);
+        Integer max = (Integer) maximum.checkMaximum();
+        Assert.assertEquals((Integer) 5, max);
     }
+
     @Test
     public void givenFloats_WhenMaxAtOne_ReturnMaximum() {
-        maximum = new FindMaximum();
-        Float max = maximum.checkMaximum(5.2f, 2.5f, 3.6f);
+        maximum = new FindMaximum(5.2f, 2.5f, 3.4f);
+        Float max = (Float) maximum.checkMaximum();
         Assert.assertEquals((Float) 5.2f, max);
     }
 
     @Test
     public void givenFloats_WhenMaxATwo_ReturnMaximum() {
-        maximum = new FindMaximum();
-        Float max = maximum.checkMaximum(3.6f, 5.2f, 2.5f);
+        maximum = new FindMaximum(2.5f, 5.2f, 3.4f);
+        Float max = (Float) maximum.checkMaximum();
         Assert.assertEquals((Float) 5.2f, max);
     }
 
     @Test
     public void givenFloats_WhenMaxAtThree_ReturnMaximum() {
-        maximum = new FindMaximum();
-        Float max = maximum.checkMaximum(2.5f, 3.6f, 5.2f);
+        maximum = new FindMaximum(2.5f, 3.4f, 5.2f);
+        Float max = (Float) maximum.checkMaximum();
         Assert.assertEquals((Float) 5.2f, max);
     }
 
     @Test
     public void givenStrings_WhenMaxAtOne_ReturnMaximum() {
-        maximum = new FindMaximum();
-        String max = maximum.checkMaximum("7", "5", "3");
+        maximum = new FindMaximum("7", "5", "1");
+        String max = (String) maximum.checkMaximum();
         Assert.assertEquals("7", max);
     }
 
     @Test
     public void givenStrings_WhenMaxAtTwo_ReturnMaximum() {
-        maximum = new FindMaximum();
-        String max = maximum.checkMaximum("5", "7", "3");
+        maximum = new FindMaximum("5", "7", "1");
+        String max = (String) maximum.checkMaximum();
         Assert.assertEquals("7", max);
     }
 
     @Test
     public void givenStrings_WhenMaxAtThree_ReturnMaximum() {
-        maximum = new FindMaximum();
-        String max = maximum.checkMaximum("3", "5", "7");
+        maximum = new FindMaximum("3", "5", "7");
+        String max = (String) maximum.checkMaximum();
         Assert.assertEquals("7", max);
     }
-
 }
+
+
